2ad84d41d0543a224f7a716ea21812fbebcc758c
andyli <43850338@qq.com>
2018-05-23 00:27:39 +0800

    liquibase

--- sample.spring.boot.parent/sample.springboot.application/src/main/java/com/ay/sample/springboot/AppStarter.java
+++ sample.spring.boot.parent/sample.springboot.application/src/main/java/com/ay/sample/springboot/AppStarter.java
@@ -4,15 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 
 import java.util.Arrays;
 
 @SpringBootApplication
-@Profile(value = "dev")
+//@Profile(value = "dev")
+//@PropertySource(value = "classpath:application.properties")
 public class AppStarter {
     private static Logger logger = LoggerFactory.getLogger(AppStarter.class);
     public static void main(String[] args) {
@@ -25,7 +28,7 @@ public class AppStarter {
 
             logger.info("a spring boot example staring");
             String[] beanNames = context.getBeanDefinitionNames();
-            Arrays.stream(beanNames).forEach(name -> logger.info(name));
+          //  Arrays.stream(beanNames).forEach(name -> logger.info(name));
         };
     }
 }
